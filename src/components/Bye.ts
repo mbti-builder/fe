2cae967ea61a81b2b18695da5a1f73d66ed1c138
dididy <dev.yongjaelee@gmail.com>
2021-09-14 21:29:53 +0900

    fix: 사용하지 않을 바닐라 컴포넌트를 제거한다
    * 제거

--- src/components/Bye.ts
+++
@@ -1,5 +0,0 @@
-export const Bye = () => {
-    const $bye = document.createElement('p');
-    $bye.innerText = 'Bye!';
-    return $bye;
-}
--- src/components/Hi.ts
+++
@@ -1,5 +0,0 @@
-export const Hi = () => {
-    const $hi = document.createElement('p');
-    $hi.innerText = 'Hi!';
-    return $hi;
-}
