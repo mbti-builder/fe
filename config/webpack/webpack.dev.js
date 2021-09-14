8027c95e135bf7785378eaaf2b22d1a04599e535
dididy <dev.yongjaelee@gmail.com>
2021-09-14 21:01:10 +0900

    fix: webpack에서 babel 및 react를 사용할 수 있도록 처리한다
    * entry 정의
    * rules에 babel-loader 추가
    * host 추가
    * 오타 수정

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -1,33 +1,42 @@
-const { merge } = require('webpack-merge');
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { merge } = require("webpack-merge");
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-const baseConfig = require('./webpack.base.js');
+const baseConfig = require("./webpack.base.js");
 const PROJECT_ROOT = path.resolve(__dirname, "../..");
 const TEMPLATE_PATH = path.resolve(PROJECT_ROOT, "public");
 
 module.exports = merge(baseConfig, {
-    mode: 'development',
-    devtool: "eval-source-map",
-    devServer: {
-        hot: true,
-        inline: true,
-        port: '5050',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s?css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: path.resolve(TEMPLATE_PATH, 'index.html'),
-            minify: {
-                removeComments: true,
-            },
-        }),
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/index.tsx",
+  devServer: {
+    hot: true,
+    inline: true,
+    port: "5050",
+    host: '127.0.0.1',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(ts|js)x?$/,
+        include: path.resolve("src"),
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(TEMPLATE_PATH, "index.html"),
+      minify: {
+        removeComments: true,
+      },
+    }),
+  ],
 });
--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -1,30 +1,38 @@
-const { merge } = require('webpack-merge');
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require("webpack-merge");
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const baseConfig = require('./webpack.base.js');
+const baseConfig = require("./webpack.base.js");
 const PROJECT_ROOT = path.resolve(__dirname, "../..");
 const TEMPLATE_PATH = path.resolve(PROJECT_ROOT, "public");
 
 module.exports = merge(baseConfig, {
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.s?css$/,
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: path.resolve(TEMPLATE_PATH, 'index.html'),
-            minify: true,
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        }),
-        new CleanWebpackPlugin(),
+  mode: "production",
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+      },
+      {
+        test: /\.(ts|js)x?$/,
+        include: path.resolve("src"),
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(TEMPLATE_PATH, "index.html"),
+      minify: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
 });
