06c48c078e99c1aa5212f36f64f38ed0f584e6dd
dididy <dev.yongjaelee@gmail.com>
2021-09-16 00:25:13 +0900

    fix: provider를 연결하고 간단한 loading 토글 예제를 추가한다

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import store from '@src/store';
+import App from './App';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
