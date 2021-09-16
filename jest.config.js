8d754789f732fba432375675e18a4c275c56f042
dididy <dev.yongjaelee@gmail.com>
2021-09-16 23:18:20 +0900

    fix: jest 에서 scss @ 키워드시 decoreator 어쩌구 에러 해결한다
    https://stackoverflow.com/questions/60026804/jest-scss-import-issue-css-modules

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    setupFilesAfterEnv: [
-      'given2/setup',
-      'jest-plugin-context/setup',
-      './jest.setup',
-    ],
-    coverageThreshold: {
-      global: {
-        branches: 100,
-        functions: 100,
-        lines: 100,
-        statements: 100,
-      },
+  setupFilesAfterEnv: [
+    'given2/setup',
+    'jest-plugin-context/setup',
+    './jest.setup',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
-    coverageReporters: ['json-summary'],
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-      '^@src(.*)$': '<rootDir>/src$1',
-    },
-  };
-  
+  },
+  coverageReporters: ['json-summary'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@src(.*)$': '<rootDir>/src$1',
+    '\\.(css|scss)$': 'identity-obj-proxy'
+  },
+};
