e13584aaf72d215f7c25594d0a38a0088076b990
SeungYoung Oh <seung-00@naver.com>
2021-09-17 05:43:18 +0900

    feature: 컴포넌트 테스트를 추가한다.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@src/test-utils';
 
 import App from './App';
 
+describe('기본 test', () => {
+  it('컴포넌트는 정상적으로 렌더링되어야 한다.', () => {
+    const { container } = render(<App />)
+    expect(container).toBeInTheDocument();
+  })
+});
 
-describe('App', () => {
-  function renderApp() {
-    return render(
-        <App />
-    );
-  }
+describe('Redux 예시 test', () => {
+  beforeEach(() => { render(<App />); })
 
-  context('컴포넌트가', () => {
-    it('정상적으로 렌더링된다', () => {
-      const { container } = renderApp();
+  it('렌더링 되었을 때 default text는 break 이어야 한다.', () => {
+    expect(screen.getByText('break')).toBeInTheDocument();
+  });
 
-      expect(container).toHaveTextContent('test');
-    });
+  it('toggle 버튼을 클릭하면 text는 loading과 break 로 번갈아 변경되어야 한다.', () => {
+    fireEvent.click(screen.getByRole('button', { name: 'toggle' }));
+    expect(screen.getByText('loading')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByRole('button', { name: 'toggle' }));
+    expect(screen.getByText('break')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByRole('button', { name: 'toggle' }));
+    expect(screen.getByText('loading')).toBeInTheDocument();
   });
 });
