89900e4b71e2a77e35d29e1c68c7f1e1f6b4db3a
dididy <dev.yongjaelee@gmail.com>
2021-09-16 20:15:12 +0900

    test: mocking 함수를 이용해 App 컴포넌트의 테스트코드를 작성한다
    * redux에 대한 부분을 정의하지 않으면 provider에 대한 오류가 발생

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
+jest.mock('react-redux');
 
-describe('App', () => {
-  function renderApp() {
-    return render(
-        <App />
+function renderApp() {
+  return render(<App />);
+}
+
+describe('<App>', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    (useDispatch as jest.MockedFunction<typeof useDispatch>).mockImplementation(
+      () => dispatch,
     );
-  }
 
-  context('컴포넌트가', () => {
-    it('정상적으로 렌더링된다', () => {
+    (useSelector as jest.MockedFunction<typeof useSelector>).mockImplementation(
+      (selector) =>
+        selector({
+          ui: { isLoading: true },
+        }),
+    );
+  });
+
+  context('컴포넌트에서', () => {
+    it('텍스트가 제대로 렌더링 되는지 확인한다', () => {
       const { container } = renderApp();
 
-      expect(container).toHaveTextContent('test');
+      expect(container).toHaveTextContent('toggleloading');
     });
   });
 });
