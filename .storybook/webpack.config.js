d969de393035a2e4fcd4caae85ff4fa169f6bc65
dididy <dev.yongjaelee@gmail.com>
2021-09-18 08:01:34 +0900

    env: scss-loader에 absolute path 및 global import 설정을 한다
    https://webpack.js.org/loaders/sass-loader/#additionaldata
    https://webpack.js.org/loaders/sass-loader/#sassoptions

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 
 const PROJECT_ROOT = path.resolve(__dirname, '../.');
 const SRC_PATH = path.resolve(PROJECT_ROOT, 'src');
@@ -6,12 +6,24 @@ const SRC_PATH = path.resolve(PROJECT_ROOT, 'src');
 module.exports = async ({ config }) => {
   config.module.rules.push({
     test: /\.s?css$/,
-    use: ["style-loader", "css-loader", "sass-loader"],
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'sass-loader',
+        options: {
+          sassOptions: {
+            includePaths: [path.join(PROJECT_ROOT, './src/styles')],
+          },
+          additionalData: `@import "index";`,
+        },
+      },
+    ],
   });
 
   config.resolve.alias = {
     '@src': SRC_PATH,
-  }
+  };
 
   // don't forget to return.
   return config;
