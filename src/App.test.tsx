56d8f9b5b4770f687a5101415242d63a137bcc70
dididy <dev.yongjaelee@gmail.com>
2021-09-20 21:59:18 +0900

    test: router처리를 반영한 App 컴포넌트 테스트코드를 작성한다

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,32 +1,38 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@src/test-utils';
+
+import { render, screen, fireEvent } from '@src/test-utils';
+
+import { MemoryRouter } from 'react-router-dom';
 
 import App from './App';
 
-describe('기본 test', () => {
-  it('컴포넌트는 정상적으로 렌더링되어야 한다.', () => {
-    const { container } = render(<App />);
-    expect(container).toBeInTheDocument();
-  });
-});
+const renderApp = ({ path }: { path: string }) => {
+  return render(
+    <MemoryRouter initialEntries={[path]}>
+      <App />
+    </MemoryRouter>,
+  );
+};
 
-describe('Redux 예시 test', () => {
-  beforeEach(() => {
-    render(<App />);
-  });
+describe('<App>', () => {
+  context('/ 경로로 이동시', () => {
+    beforeEach(() => {
+      renderApp({ path: '/' });
+    });
 
-  it('렌더링 되었을 때 default text는 break 이어야 한다.', () => {
-    expect(screen.getByText('break')).toBeInTheDocument();
-  });
+    it('렌더링 되었을 때 default text는 break 이어야 한다.', () => {
+      expect(screen.getByText('toggle')).toBeInTheDocument();
+    });
 
-  it('toggle 버튼을 클릭하면 text는 loading과 break 로 번갈아 변경되어야 한다.', () => {
-    fireEvent.click(screen.getByRole('button', { name: 'toggle' }));
-    expect(screen.getByText('loading')).toBeInTheDocument();
+    it('toggle 버튼을 클릭하면 text는 loading과 break 로 번갈아 변경되어야 한다.', () => {
+      fireEvent.click(screen.getByRole('button', { name: 'toggle' }));
+      expect(screen.getByText('loading')).toBeInTheDocument();
 
-    fireEvent.click(screen.getByRole('button', { name: 'toggle' }));
-    expect(screen.getByText('break')).toBeInTheDocument();
+      fireEvent.click(screen.getByRole('button', { name: 'toggle' }));
+      expect(screen.getByText('break')).toBeInTheDocument();
 
-    fireEvent.click(screen.getByRole('button', { name: 'toggle' }));
-    expect(screen.getByText('loading')).toBeInTheDocument();
+      fireEvent.click(screen.getByRole('button', { name: 'toggle' }));
+      expect(screen.getByText('loading')).toBeInTheDocument();
+    });
   });
 });
