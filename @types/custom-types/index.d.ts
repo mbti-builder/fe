c4b885249686a9d0c830fe41483a66a92c0ae6fa
raejoonee <crj0901@gmail.com>
2021-09-21 16:49:43 +0900

    env: 사용자 정의 타입을 관리하는 index.d.ts 파일을 생성한다.
    https://kjwsx23.tistory.com/522

---
+++ @types/custom-types/index.d.ts
@@ -0,0 +1,7 @@
+declare module 'custom-types' {
+  export namespace customTypes {
+    type ElementSize = 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+    type ElementVariant = 'ghost' | 'solid' | 'quiet' | 'minimal';
+    type ElementColor = 'black' | 'gray' | 'blue' | 'red';
+  }
+}
